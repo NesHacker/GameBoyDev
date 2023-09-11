461635571b9f154dff111ee5f3a5e0086fe34a95
Ryan <ryan@neshacker.com>
2023-09-11 13:57:09 -0400

    Added DMA and BG animations.

--- src/main.asm
+++ src/main.asm
@@ -1,6 +1,14 @@
 INCLUDE "hardware.inc"
 
-; Address for the first byte of level data in RAM
+DEF ary_SpriteOAM EQU $C100
+DEF len_SpriteOAM EQU 160
+
+DEF i_BgTimer EQU $C080
+DEF i_BgFrame EQU $C081
+DEF BgDelay EQU 18
+
+DEF ary_Onigiri EQU $C300
+
 DEF ary_LevelData EQU $D000
 
 SECTION "Header", ROM0[$100]
@@ -8,6 +16,7 @@ SECTION "Header", ROM0[$100]
 	ds $150 - @, 0	; Header space. RGBFIX requires that it be zero filled.
 
 SECTION "Main", ROM0
+
 ; ------------------------------------------------------------------------------
 ; `func Main()`
 ;
@@ -16,8 +25,70 @@ SECTION "Main", ROM0
 ; ------------------------------------------------------------------------------
 Main:
 	call SetupGame
-.gameloop
-	jp .gameloop
+.loop
+	ld a, [rLY]
+	cp 144
+	jp nc, .loop
+.vblank_wait
+	ld a, [rLY]
+	cp 144
+	jp c, .vblank_wait
+	call GameLoop
+	jp .loop
+
+GameLoop:
+	call AnimateBackground
+	ret
+
+; ------------------------------------------------------------------------------
+; `func AnimateBg()`
+;
+; Handles timing and updates for background animations.
+; ------------------------------------------------------------------------------
+AnimateBackground:
+	ld a, [i_BgTimer]
+	dec a
+	ld [i_BgTimer], a
+	jp nz, .skip
+	ld a, BgDelay
+	ld [i_BgTimer], a
+	ld a, [i_BgFrame]
+	xor 1
+	ld [i_BgFrame], a
+	call UpdateOnigiriSprites
+.skip
+	ret
+
+; ------------------------------------------------------------------------------
+; `func UpdateOnigiriSprites`
+;
+; Animates the onigiri sprites in the background by swapping between the two
+; tilesets.
+; ------------------------------------------------------------------------------
+UpdateOnigiriSprites:
+	ld hl, ary_Onigiri
+.update_loop
+	ld a, [hli]
+	or 0
+	jr z, .return
+	ld d, a
+	ld a, [hli]
+	ld e, a
+	ld a, [de]
+	and $0F
+	sub $0C
+	add 2
+	and $03
+	add $0C
+	ld b, a
+	ld a, [de]
+	and $F0
+	or b
+	ld [de], a
+	jr .update_loop
+.return
+	ret
+
 
 SECTION "Setup", ROM0
 
@@ -36,18 +107,52 @@ SetupGame:
 	ld a, 0
 	ld [rLCDC], a
 
+	; Clear WRAM
+	ld bc, $2000
+	ld hl, $C000
+.clear_loop
+	ld a, 0
+	ld [hli], a
+	dec bc
+	ld a, b
+	or a, c
+	jr nz, .clear_loop
+
+	; Load the DMA Transfer Routine
+	call LoadDMARoutine
+
 	; Load all level related data
 	call LoadLevelData
+
+	; Intialize the two frame animator for onigiri sprites
+	ld a, BgDelay
+	ld [i_BgTimer], a
+	ld a, 0
+	ld [i_BgFrame], a
+
+	; Clear the Sprite OAM data
+	ld a, 0
+	ld b, len_SpriteOAM
+	ld hl, ary_SpriteOAM
+.sprite_clear_loop
+  ld [hli], a
+	dec b
+	jr nz, .sprite_clear_loop
+
+	; Transfer the sprite data using DMA
+	call DMATransfer
 
 	; Initialize the screen position
 	ld a, 112
 	ld [rSCY], a
 
 	; Turn the LCD back on and initialize display registers
-	ld a, LCDCF_ON | LCDCF_BGON
+	ld a, LCDCF_ON | LCDCF_BGON | LCDCF_OBJON | LCDCF_OBJ16
 	ld [rLCDC], a
 	ld a, %11100100
 	ld [rBGP], a
+	ld [rOBP0], a
+	ld [rOBP1], a
 	ret
 
 ; ------------------------------------------------------------------------------
@@ -88,7 +193,87 @@ LoadLevelData:
 	ld de, LevelData
 	ld hl, ary_LevelData
 	call LoadData
+	call FindOnigiri
 	ret
+
+; ------------------------------------------------------------------------------
+; `func FindOnigiri()`
+;
+; Finds the addresses for all background tiles that depict onigiri so they can
+; be animated quickly in the main game loop. Normally this list would be
+; compiled into the level data prior, but I am just doing something simple here.
+; ------------------------------------------------------------------------------
+FindOnigiri:
+	ld bc, $9800
+	ld de, LevelData
+	ld hl, ary_Onigiri
+.loop
+	ld a, [de]
+	cp a, 5
+	jr nz, .skip
+	ld a, b
+	ld [hli], a
+	ld a, c
+	ld [hli], a
+.skip
+	inc de
+	inc bc
+	ld a, d
+	cp a, HIGH(LevelData + len_LevelData)
+	jr nz, .loop
+	ld a, e
+	cp a, LOW(LevelData + len_LevelData)
+	jr nz, .loop
+	ret
+
+; ------------------------------------------------------------------------------
+; `func DMATransfer()`
+;
+; Transfers sprites from WRAM to VRAM using the DMA.
+; ------------------------------------------------------------------------------
+DEF DMATransfer EQU $FF80
+
+; ------------------------------------------------------------------------------
+; `func LoadDMARoutine()`
+;
+; Loads the DMA transfer routine into memory starting at address $FF80. For
+; more information see the explanation in the documentation for the
+; `DMATransferRoutine` function below.
+; ------------------------------------------------------------------------------
+LoadDMARoutine:
+	ld b, DMATransferRoutineEnd - DMATransferRoutine
+	ld de, DMATransferRoutine
+	ld hl, DMATransfer
+.load_loop
+	ld a, [de]
+	inc de
+	ld [hli], a
+	dec b
+	jr nz, .load_loop
+	ret
+
+; ------------------------------------------------------------------------------
+; `func DMATransferRoutine()`
+;
+; This is the DMA transfer routine used to quickly copy sprite object data from
+; working RAM to video RAM.
+;
+; **IMPORTANT:** This routine should not be called directly, in order to prevent
+; bus conflicts the Game Boy only executes instructions between $FF80-$FFFE
+; during a DMA transfer. As such this routine is copied to that memory region
+; and you should call it using the `DMATransfer` routine label instead.
+; ------------------------------------------------------------------------------
+DMATransferRoutine:
+	di
+	ld a, $C1
+	ld [rDMA], a
+	ld a, 40
+.wait_loop
+	dec a
+	jr nz, .wait_loop
+	ei
+	ret
+DMATransferRoutineEnd:
 
 SECTION "Tile data", ROM0
 
